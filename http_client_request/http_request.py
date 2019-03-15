c1905dcceb027606b4591c95204c90c99b645be9
chensheng <chensheng@youzan.com>
2019-03-15 08:07:11 +0800

    python3 的http请求

--- http_client_request/http_request.py
+++ http_client_request/http_request.py
@@ -1,45 +1,46 @@
-# -*- coding:utf-8 -*-
-import urllib2, urllib, cookielib, json
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
 
-# http请求的通用方法
+import urllib.request
+import urllib.parse
+import http.cookiejar
+import json
+from bs4 import BeautifulSoup
 
 class HttpRequest:
-    def __init__(self, proxy=None):
-        cj = cookielib.CookieJar()
-        if proxy is not None:
-            proxy_support = urllib2.ProxyHandler(proxy)
-            self.opener = urllib2.build_opener(
-                proxy_support, urllib2.HTTPCookieProcessor(cj))
-        else:
-            self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    def __init__(self):
+        cookie = http.cookiejar.CookieJar()
+        #实例化一个全局opener
+        cookie_handler = urllib.request.HTTPCookieProcessor(cookie)
+        self.opener=  urllib.request.build_opener(cookie_handler)
 
-    def get_data(self, url, data=None):
+    def getData(self, url, data = None):
         if data is None:
             url = url
         else:
-            params = urllib.urlencode(data)
+            params = urllib.parse.urlencode(data)
             url = url + '?' + params
-
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
-            # 'Accept-Encoding': 'gzip, deflate, sdch',
-            # 'Accept-Language': 'zh-CN,zh;q=0.8',
-            # 'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
-        }
-##        print url
-        req = urllib2.Request(url, headers=headers)
-        res_data = self.opener.open(req, timeout=20)  # urllib2.urlopen(req)
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'no-cache',
+                'Connection': 'keep-alive'
+            }
+        # req = urllib.request.Request(url, headers = headers, method='GET');
+        # res_data = urllib.request.urlopen(url)
+        res_data = self.opener.open(url, timeout = 20)
         return res_data
 
-    # dataType formdata/text/json/xml
-    def post_data(self, url, data={}, header={}, dataType='formdata'):
+    # dataType form/text/json
+    def postData(self, url, data = {}, header = {}, dataType = 'form', encoding = 'utf-8'):
         initHeader = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         }
-        if dataType == 'formdata':
+        if dataType == 'form':
             if isinstance(data, dict):
-                data = urllib.urlencode(data)
+                data = urllib.parse.urlencode(data)
             tempHeader = {
                 'Content-Type': 'application/x-www-form-urlencoded;'
             }
@@ -50,18 +51,34 @@ class HttpRequest:
                 'Content-Type': 'application/json;'
             }
         elif dataType == 'plain':
-            data = str(data)
             tempHeader = {
                 'Content-Type': 'text/plain;'
             }
+        data = data.encode(encoding, 'ignore')
         tempHeader['Content-Length'] = len(data)
-        headers = dict(initHeader.items() +
-                       tempHeader.items() + header.items())
+        headers = dict(initHeader)
+        headers.update(tempHeader)
+        headers.update(header)
 
-        req = urllib2.Request(url, data, headers=headers)
+        req = urllib.request.Request(url, data, headers = headers, method='POST');
         res_data = self.opener.open(req)
         return res_data
 
+    def bs4HttpData(self, data):
+        bsHandle = BeautifulSoup(data, 'lxml')
+        return bsHandle
+
 if __name__ == '__main__':
+
+    q = {
+        'wd': '测试'
+    }
     t = HttpRequest()
-    print t.get_data('https://www.baidu.com').read()
+    print (t.getData('http://www.baidu.com/s?wd=%E6%B5%8B%E8%AF%95').read().decode('utf8', 'ignore'))
+
+    data = {
+            'identity': '',
+            'password': ''
+        };
+    res = t.postData('https://www.itjuzi.com/user/login', data = data, dataType = 'form')
+    print (res.info())
